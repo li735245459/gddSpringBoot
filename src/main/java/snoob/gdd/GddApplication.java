3e9c84c22a9f554620884e48834dc426ddfc3d2d
li735245459 <lixing_java@163>
2018-05-29 15:08:19 +0800

    first commit

---
+++ src/main/java/snoob/gdd/GddApplication.java
@@ -0,0 +1,12 @@
+package snoob.gdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GddApplication.class, args);
+    }
+}
