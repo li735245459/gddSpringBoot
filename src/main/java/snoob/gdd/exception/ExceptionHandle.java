a2c697a362eb795d86d65e99a2d2d0a5412a4ff5
li735245459 <lixing_java@163>
2018-06-06 09:43:07 +0800

    Default

--- src/main/java/snoob/gdd/exception/ExceptionHandle.java
+++ src/main/java/snoob/gdd/exception/ExceptionHandle.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import snoob.gdd.enums.ResultEnum;
 import snoob.gdd.model.Result;
 import snoob.gdd.util.ResultUtil;
@@ -13,26 +14,25 @@ import java.text.MessageFormat;
 
 /**
  * 全局Controller异常捕获模块: 捕获controller抛出的异常
- *  service接口需要抛出异常
  */
-@ControllerAdvice
+//@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionHandle {
 
     private final static Logger logger = LoggerFactory.getLogger(ExceptionHandle.class);
+
     @ExceptionHandler(value = Exception.class)
-    @ResponseBody
+//    @ResponseBody
     public Result handle(Exception e) {
         if (e instanceof GddException) {
             GddException gddException = (GddException) e;
-            /**
-             * 将所有打印的日志统一打印到这里
-             */
             // 入库操作
+            logger.error(MessageFormat.format(gddException.getCode().toString(), gddException.getMessage()));
             return ResultUtil.error(gddException.getCode(), gddException.getMessage());
         } else {
             // 入库操作
-            logger.debug(MessageFormat.format("【系统错误】{0}--", e.getMessage()));
-            return ResultUtil.error(ResultEnum.SYSTEM_ERROR.getCode(), ResultEnum.SYSTEM_ERROR.getMsg());
+            logger.error(MessageFormat.format("【系统错误】{0}--", e.getMessage()));
+            return ResultUtil.error(ResultEnum.SYSTEM_ERROR.getCode(), e.getMessage());
         }
     }
 }
